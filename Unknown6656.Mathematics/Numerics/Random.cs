82db9ccaaae76486c796766d7eae1ecda3007a54
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-08-01 03:10:43 +0200

    refactored the random class

--- Unknown6656.Mathematics/Numerics/Random.cs
+++ Unknown6656.Mathematics/Numerics/Random.cs
@@ -1,25 +1,22 @@
-﻿using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Collections.Generic;
+﻿using System.Security.Cryptography;
 using System.Linq;
-using System.IO;
 using System;
 
 using Unknown6656.Mathematics.LinearAlgebra;
 using Unknown6656.Mathematics.Cryptography;
 using Unknown6656.Mathematics.Analysis;
 using Unknown6656.Generics;
-using Unknown6656.Common;
 
 using netrandom = System.Random;
 
 namespace Unknown6656.Mathematics.Numerics;
 
 
-public abstract unsafe class Random
+public abstract class Random
+    : netrandom
 {
-    public static BuiltinRandom BuiltinRandom { get; } = new BuiltinRandom();
-    public static XorShift XorShift { get; } = new XorShift();
+    public static Random BuiltinRandom { get; } = new _builtin();
+    public static Random XorShift { get; } = new XorShift();
 
     public long Seed { get; }
 
@@ -30,58 +27,153 @@ public abstract unsafe class Random
     }
 
     public Random(long seed)
+        : base(seed.GetHashCode())
     {
         Seed = seed;
+
         Init();
     }
 
+
     protected abstract void Init();
 
-    public byte NextByte() => (byte)(NextInt() & 0xff);
+    public abstract override int Next();
 
-    public sbyte NextSByte() => (sbyte)NextByte();
-
-    public char NextChar() => (char)NextShort();
-
-    public short NextShort() => (short)NextUShort();
-
-    public ushort NextUShort() => (ushort)(NextInt() & 0xffff);
-
-    public float NextFloat() => (float)NextDouble();
-
-    public double NextDouble() => ((double)NextULong() / long.MaxValue) % 1d;
-
-    public decimal NextDecimal() => (decimal)NextDouble();
-
-    public Scalar NextScalar() => NextDouble();
+    #region BOOLEAN
 
     public bool NextBool() => NextBool(.5);
 
     public bool NextBool(Scalar true_probability) => NextDouble() >= true_probability;
 
+    public T Choose<T>(T left, T right) => Choose(left, right, .5);
+
+    public T Choose<T>(T left, T right, Scalar right_probability) => NextBool(right_probability) ? right : left;
+
+    public ref T Choose<T>(ref T left, ref T right) => ref Choose(ref left, ref right, .5);
+
+    public ref T Choose<T>(ref T left, ref T right, Scalar right_probability) => ref NextBool(right_probability) ? ref right : ref left;
+
+    public ref T Choose<T>(params T[] items) => ref items[Next(items.Length)];
+
+    #endregion
+    #region INTEGER
+
+    public byte NextByte() => (byte)(Next() & 0xff);
+
+    public sbyte NextSByte() => (sbyte)NextByte();
+
+    public char NextChar() => (char)NextUInt16();
+
+    public short NextInt16() => (short)NextUInt16();
+
+    public ushort NextUInt16() => (ushort)(Next() & 0xffff);
+
+    public override int Next(int max) => (int)((uint)Next() / ((float)uint.MaxValue + 1) * max);
+
+    public override int Next(int min, int max) => min + Next(max - min);
+
+    public int NextInt32() => Next();
+
+    public int NextInt32(int max) => Next(max);
+
+    public int NextInt32(int min, int max) => Next(min, max);
+
+    public uint NextUInt32() => (uint)Next();
+
+    public override long NextInt64() => NextBinary<long>();
+
+    public override long NextInt64(long max) => (long)((ulong)NextInt64() / ((double)ulong.MaxValue + 1) * max);
+
+    public override long NextInt64(long min, long max) => min + NextInt64(max - min);
+
+    public ulong NextUInt64() => NextBinary<ulong>();
+
+    public UInt128 NextUInt128() => NextBinary<UInt128>();
+
+    #endregion
+    #region FLOATING POINT
+
+    public Half NextHalf() => (Half)NextDouble();
+
+    public override float NextSingle() => (float)NextDouble();
+
+    public override double NextDouble() => ((double)NextUInt64() / long.MaxValue) % 1d;
+
+    protected override double Sample() => NextDouble();
+
+    public decimal NextDecimal() => (decimal)NextDouble();
+#if F16
+    public Scalar NextScalar() => NextHalf();
+#elif F32
+    public Scalar NextScalar() => NextSingle();
+#elif F64
+    public Scalar NextScalar() => NextDouble();
+#endif
+    public Scalar NextScalar(Scalar max) => NextScalar() * max;
+
+    public Scalar NextScalar(Scalar min, Scalar max) => min + NextScalar(max - min);
+
+    public Complex NextComplex() => new(NextScalar(), NextScalar());
+
+    public Complex NextComplex(Scalar length) => NextComplex().Normalized * length;
+
+    #endregion
+    #region SEQUENCES
+
+    public byte[] NextBytes(int count)
+    {
+        byte[] buffer = new byte[count];
+
+        NextBytes(buffer);
+
+        return buffer;
+    }
+
+    public override void NextBytes(byte[] buffer) => NextBytes(buffer, 0, buffer.Length);
+
+    public void NextBytes(byte[] buffer, int offset) => NextBytes(buffer, offset, buffer.Length - offset);
+
+    public void NextBytes(byte[] buffer, int offset, int length) => NextBytes(new Span<byte>(buffer), offset, length);
+
+    public override void NextBytes(Span<byte> buffer) => NextBytes(buffer, 0, buffer.Length);
+
+    public void NextBytes(Span<byte> buffer, int offset) => NextBytes(buffer, offset, buffer.Length - offset);
+
+    public virtual unsafe void NextBytes(Span<byte> buffer, int offset, int length)
+    {
+        fixed (byte* bptr = buffer)
+        {
+            int* iptr = (int*)(bptr + offset);
+            int i = 0;
+
+            while (i < length)
+                if (length - i > 4)
+                {
+                    iptr[i / 4] = Next();
+                    i += 4;
+                }
+                else
+                    bptr[i++ + offset] = NextByte();
+        }
+    }
+
     public sbyte[] NextSBytes(int count) => Enumerable.Range(0, count).ToArray(_ => NextSByte());
 
     public char[] NextChars(int count) => Enumerable.Range(0, count).ToArray(_ => NextChar());
 
-    public short[] NextShorts(int count) => Enumerable.Range(0, count).ToArray(_ => NextShort());
+    public short[] NextInt16s(int count) => Enumerable.Range(0, count).ToArray(_ => NextInt16());
 
-    public ushort[] NextUShorts(int count) => Enumerable.Range(0, count).ToArray(_ => NextUShort());
+    public ushort[] NextUInt16s(int count) => Enumerable.Range(0, count).ToArray(_ => NextUInt16());
 
-    public float[] NextFloats(int count) => Enumerable.Range(0, count).ToArray(_ => NextFloat());
+    public Half[] NextHalves(int count) => Enumerable.Range(0, count).ToArray(_ => NextHalf());
+
+    public float[] NextSingles(int count) => Enumerable.Range(0, count).ToArray(_ => NextSingle());
 
     public double[] NextDoubles(int count) => Enumerable.Range(0, count).ToArray(_ => NextDouble());
 
     public decimal[] NextDecimals(int count) => Enumerable.Range(0, count).ToArray(_ => NextDecimal());
 
-    public Scalar[] NextScalars(int count) => Enumerable.Range(0, count).ToArray(_ => NextScalar());
-
-    public bool[] NextBools(int count) => Enumerable.Range(0, count).ToArray(_ => NextBool());
-
-    public bool[] NextBools(int count, Scalar true_probability) => Enumerable.Range(0, count).ToArray(_ => NextBool(true_probability));
-
-    public ref T Choose<T>(ref T left, ref T right) => ref Choose(ref left, ref right, .5);
-
-    public ref T Choose<T>(ref T left, ref T right, Scalar right_probability) => ref NextBool(right_probability) ? ref right : ref left;
+    public Half NextGaussian(Half mean, Half deviation) => (Half)NextGaussian((double)mean, (double)deviation);
 
     public float NextGaussian(float mean, float deviation) => (float)NextGaussian((double)mean, deviation);
 
@@ -105,6 +197,8 @@ public abstract unsafe class Random
 
     public double[] NextGaussians(int count, double mean, double deviation) => Enumerable.Range(0, count).ToArray(_ => NextGaussian(mean, deviation));
 
+    public Scalar[] NextScalars(int count) => Enumerable.Range(0, count).ToArray(_ => NextScalar());
+
     public Scalar[] NextScalars(int count, Scalar max) => Enumerable.Range(0, count).ToArray(_ => NextScalar(max));
 
     public Scalar[] NextScalars(int count, Scalar min, Scalar max) => Enumerable.Range(0, count).ToArray(_ => NextScalar(min, max));
@@ -113,97 +207,68 @@ public abstract unsafe class Random
 
     public Complex[] NextComplexes(int count) => Enumerable.Range(0, count).ToArray(_ => NextComplex());
 
-    public int[] NextInts(int count) => Enumerable.Range(0, count).ToArray(_ => NextInt());
+    public int[] NextInt32s(int count) => Enumerable.Range(0, count).ToArray(_ => NextInt32());
 
-    public uint[] NextUInts(int count) => Enumerable.Range(0, count).ToArray(_ => NextUInt());
+    public uint[] NextUInt32s(int count) => Enumerable.Range(0, count).ToArray(_ => NextUInt32());
 
-    public long[] NextLongs(int count) => Enumerable.Range(0, count).ToArray(_ => NextLong());
+    public long[] NextInt64s(int count) => Enumerable.Range(0, count).ToArray(_ => NextInt64());
 
-    public ulong[] NextULongs(int count) => Enumerable.Range(0, count).ToArray(_ => NextULong());
+    public ulong[] NextUInt64s(int count) => Enumerable.Range(0, count).ToArray(_ => NextUInt64());
 
     public UInt128[] NextUInt128s(int count) => Enumerable.Range(0, count).ToArray(_ => NextUInt128());
 
-    public Scalar NextScalar(Scalar max) => NextScalar() * max;
+    public bool[] NextBools(int count) => Enumerable.Range(0, count).ToArray(_ => NextBool());
 
-    public Scalar NextScalar(Scalar min, Scalar max) => min + NextScalar(max - min);
+    public bool[] NextBools(int count, Scalar true_probability) => Enumerable.Range(0, count).ToArray(_ => NextBool(true_probability));
 
-    public Complex NextComplex() => new(NextScalar(), NextScalar());
+    #endregion
+    #region GENERICS
 
-    public Complex NextComplex(Scalar length) => NextComplex().Normalized * length;
-
-    public abstract int NextInt();
-
-    public int NextInt(int max) => (int)(NextUInt() / ((float)uint.MaxValue + 1) * max);
-
-    public int NextInt(int min, int max) => min + NextInt(max - min);
-
-    public uint NextUInt() => (uint)NextInt();
-
-    public long NextLong() => Next<long>();
-
-    public ulong NextULong() => Next<ulong>();
-
-    public UInt128 NextUInt128() => Next<UInt128>();
-
-    public byte[] NextBytes(int count) => Fill(new byte[count]);
-
-    public byte[] Fill(byte[] buffer) => Fill(buffer, 0, buffer.Length);
-
-    public byte[] Fill(byte[] buffer, int offset) => Fill(buffer, offset, buffer.Length - offset);
-
-    public virtual byte[] Fill(byte[] buffer, int offset, int length)
-    {
-        fixed (byte* bptr = buffer ??= new byte[length + offset])
-        {
-            int* iptr = (int*)(bptr + offset);
-            int i = 0;
-
-            while (i < length)
-                if (length - i > 4)
-                {
-                    iptr[i / 4] = NextInt();
-                    i += 4;
-                }
-                else
-                    bptr[i++ + offset] = NextByte();
-        }
-
-        return buffer;
-    }
-
-    public IEnumerable<T> Shuffle<T>(IEnumerable<T> collection) => collection.Shuffle(this);
-
-    public ref T Choose<T>(params T[] items) => ref items[NextInt(items.Length)];
-
-    public T Next<T>()
+    public unsafe T NextBinary<T>()
         where T : unmanaged
     {
         T res = default;
+        byte* ptr = (byte*)&res;
+        int remaining = sizeof(T);
 
-        if (sizeof(T) % 4 == 0)
-        {
-            int* ptr = (int*)&res;
-
-            for (int i = 0, s = sizeof(T) / 4; i < s; ++i)
-                ptr[i] = NextInt();
-        }
-        else
-        {
-            byte* ptr = (byte*)&res;
-
-            for (int i = 0, s = sizeof(T); i < s; ++i)
-                ptr[i] = NextByte();
-        }
+        while (remaining > 0)
+            if (remaining >= sizeof(int))
+            {
+                *(int*)ptr = Next();
+                ptr += sizeof(int);
+                remaining -= sizeof(int);
+            }
+            else
+            {
+                *ptr = NextByte();
+                ++ptr;
+                --remaining;
+            }
 
         return res;
     }
 
-    public T[] Next<T>(int count) where T : unmanaged => Enumerable.Range(0, count).ToArray(_ => Next<T>());
+    public T[] NextBinary<T>(int count) where T : unmanaged => Enumerable.Range(0, count).ToArray(_ => NextBinary<T>());
 
-    public static implicit operator Random(netrandom random) => new BuiltinRandom._(random);
+    #endregion
 
-    public static implicit operator Stream(Random random) => ByteGeneratorStream.FromRandom(random);
 
+    private sealed class _builtin
+        : Random
+    {
+        private readonly netrandom _random;
+
+
+        public _builtin() : base() => _random = new();
+
+        public _builtin(long seed) : base(seed) => _random = new((int)seed);
+
+        protected override void Init()
+        {
+        }
+
+        public override int Next() => _random.Next();
+    }
 }
 
 public sealed class XorShift
@@ -218,7 +283,7 @@ public sealed class XorShift
     }
 
     public XorShift(long seed)
-        : base(seed)
+        : base((int)seed)
     {
     }
 
@@ -231,15 +296,18 @@ public sealed class XorShift
 
         if (_x == 0)
             _x = 0x6a598431;
+
         if (_y == 0)
             _y = 0x94b86cf9;
+
         if (_z == 0)
             _z = (uint)(_x ^ Seed ^ _y);
+
         if (_w == 0)
             _w = _x.ROL((int)_y) ^ _y.ROL((int)_x) + 1;
     }
 
-    public override int NextInt()
+    public override int Next()
     {
         uint s = _x;
         uint t = _w;
@@ -264,56 +332,24 @@ public sealed class CongruenceGenerator
 
     protected override void Init() => (_x, _) = TextbookRSA.GenerateKeyPair();
 
-    public override int NextInt() => (_x = _x.Power(2)).GetHashCode();
+    public override int Next() => (_x = _x.Power(2)).GetHashCode();
 }
 
-public class BuiltinRandom
-    : Random
-{
-    private netrandom _random;
-
-
-    public BuiltinRandom()
-        : base() => _random = new netrandom((int)Seed);
-
-    public BuiltinRandom(int seed)
-        : base(seed) => _random = new netrandom((int)Seed);
-
-    protected override void Init() => _random = new netrandom((int)Seed);
-
-    public override int NextInt() => _random.Next();
-
-    public static implicit operator netrandom(BuiltinRandom random) => random._random;
-
-
-    internal sealed class _
-        : BuiltinRandom
-    {
-        public _(netrandom r)
-            : base() => _random = r;
-
-        protected override void Init()
-        {
-        }
-    }
-}
-
-public sealed class CryptoRandom<RNG>
+public sealed class CryptoRandom<RNG>(RNG rng)
     : Random
     where RNG : RandomNumberGenerator
 {
-    public RNG RandomNumberGenerator { get; }
+    public RNG RandomNumberGenerator { get; } = rng;
 
 
-    public CryptoRandom(RNG rng) => RandomNumberGenerator = rng;
-
-    public override int NextInt()
+    public override unsafe int Next()
     {
         byte[] bytes = new byte[sizeof(int)];
 
         RandomNumberGenerator.GetBytes(bytes);
 
-        return bytes.BinaryCast<int>();
+        fixed (byte* ptr = bytes)
+            return *(int*)ptr;
     }
 
     protected override void Init()
@@ -339,7 +375,7 @@ public sealed class TurbulenceShift
 
     protected override void Init() => _state = Seed.GetHashCode();
 
-    public override int NextInt()
+    public override int Next()
     {
         uint i = (uint)_state;
 
